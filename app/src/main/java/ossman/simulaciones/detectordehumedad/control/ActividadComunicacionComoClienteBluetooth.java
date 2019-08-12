1d772e682ea90c21e3280ec072b6794c1978090a
ossman <oocarvajalt@unal.edu.co>
2019-08-12 17:02:37 -0500

    Casi termino de agregar toda esa mierda

---
+++ app/src/main/java/ossman/simulaciones/detectordehumedad/control/ActividadComunicacionComoClienteBluetooth.java
@@ -0,0 +1,12 @@
+package ossman.simulaciones.detectordehumedad.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadComunicacionComoClienteBluetooth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
